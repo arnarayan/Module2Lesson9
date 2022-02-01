f295eee476064b616dec7ff071a3271756bba9f2
Anand.Narayan <arnarayan@gmail.com>
2022-01-31 21:52:20 -0700

    Initial Commit

---
+++ Module2Lesson9/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Module2Lesson9
+//
+//  Created by Anand Narayan on 2022-01-31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Module2Lesson9/Module2Lesson9App.swift
@@ -0,0 +1,17 @@
+//
+//  Module2Lesson9App.swift
+//  Module2Lesson9
+//
+//  Created by Anand Narayan on 2022-01-31.
+//
+
+import SwiftUI
+
+@main
+struct Module2Lesson9App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
